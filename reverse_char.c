24195c76b7fd276363be35abc04f4e675fc17cb1
Kitthinut <110830551+Kitthinut@users.noreply.github.com>
2023-06-19 21:43:09 +0700

    Create reverse_char.c

---
+++ reverse_char.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    
+    char c1='B';
+    char c2='A';
+    char c3='M';
+    
+    printf("The reverse of %c%c%c is %c%c%c\n",
+        c1, c2, c3,
+        c3, c2, c1);
+
+    return 0;
+}
